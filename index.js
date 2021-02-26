65d476241ba49b46ed12c1916939052b7fc513f4
osxgirl <osxgirl@mac.com>
2021-02-26 14:13:57 -0500

    commit tree 83650b1ce08f7990a661d4face3a5b8816c48027

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 62
+const num2 = 1
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const mod = 19 % 5
+
+const max = Math.max(2, 3, 8, 19, 20)
